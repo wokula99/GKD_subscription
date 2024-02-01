ea8eef49463a00e5aa51a55c1edb4a94f2a6b4f8
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-02-01 17:27:54 +0800

    feat(剪映): 局部广告-选择素材界面上方广告

--- src/apps/com.lemon.lv.ts
+++ src/apps/com.lemon.lv.ts
@@ -6,7 +6,7 @@ export default defineAppConfig({
   groups: [
     {
       key: 0,
-      name: '导出界面底部广告',
+      name: '局部广告-导出界面底部广告',
       desc: '关闭完成导出后的下方广告',
       activityIds: 'com.vega.export.edit.view.ExportActivity',
       rules: [
@@ -15,6 +15,14 @@ export default defineAppConfig({
           snapshotUrls: 'https://i.gkd.li/import/12911010',
         },
       ],
+    },
+    {
+      key: 1,
+      name: '局部广告-选择素材界面上方广告',
+      activityIds: 'com.vega.libcutsame.select.view.CutSameSelectMediaActivity',
+      rules: 
+        '[vid="gallery_layout_below_category_view"] >3 [text="立即下载"] -n LynxFlattenUI[clickable=true]',
+      snapshotUrls: 'https://i.gkd.li/import/14122865',
     },
   ],
 });
