ab957f661481327e5206a1f6d6d57acc1b1227d3
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-02-02 23:16:38 +0800

    feat(JMComic2): 开屏广告、局部广告-首页底部广告、功能类-已满18岁弹窗、功能类-签到活动

---
+++ src/apps/com.jiaohua_browser.ts
@@ -0,0 +1,41 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.jiaohua_browser',
+  name: 'JMComic2',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      resetMatch: 'app',
+      actionMaximum: 1,
+      rules: '@ViewGroup > [text=""]',
+      snapshotUrls: 'https://i.gkd.li/import/14141773',
+    },
+    {
+      key: 1,
+      name: '局部广告-首页底部广告',
+      activityIds: 'com.jiaohua_browser.MainActivity',
+      rules: '@ViewGroup > [text=""]',
+      snapshotUrls: 'https://i.gkd.li/import/14141810',
+    },
+    {
+      key: 2,
+      name: '功能类-已满18岁弹窗',
+      quickFind: true,
+      activityIds: 'com.jiaohua_browser.MainActivity',
+      rules: '[text*="滿18歲"]',
+      snapshotUrls: 'https://i.gkd.li/import/14141809',
+    },
+    {
+      key: 3,
+      name: '功能类-签到活动',
+      quickFind: true,
+      activityIds: 'com.jiaohua_browser.MainActivity',
+      rules: '@ViewGroup > [text="立即簽到"]',
+      snapshotUrls: 'https://i.gkd.li/import/14141807',
+    },
+  ],
+});
