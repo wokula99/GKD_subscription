c1f809ffb28e77b4753cd96c1c5fe7d5c657ed92
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-02-03 00:03:42 +0800

    feat(到梦空间): 功能类-学习活动

---
+++ src/apps/com.jingcai.apps.qualitydev.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.jingcai.apps.qualitydev',
+  name: '到梦空间',
+  groups: [
+    {
+      key: 1,
+      name: '功能类-学习活动',
+      quickFind: true,
+      activityIds: 'com.jingcai.apps.qualitydev.qualitync.activity_home.ActivityMainTab',
+      rules: '[text="立即参与"][vid="tv_next"] -2 [text="关闭"][vid="tv_step"]',
+      snapshotUrls: 'https://i.gkd.li/import/14054117',
+    },
+  ],
+});
