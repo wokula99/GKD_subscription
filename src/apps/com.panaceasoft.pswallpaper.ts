6934a64477fbd2c45d167c341a83f15725c06161
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-02-02 22:53:05 +0800

    feat(ClrvAI): 功能类-公告弹窗

---
+++ src/apps/com.panaceasoft.pswallpaper.ts
@@ -0,0 +1,18 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.panaceasoft.pswallpaper',
+  name: 'ClrvAI',
+  groups: [
+    {
+      key: 1,
+      name: '功能类-公告弹窗',
+      quickFind: true,
+      matchTime: 10000,
+      resetMatch: 'app',
+      actionMaximum: 1,
+      rules: '[vid="tv_next"][text="下一条"] <<n LinearLayout - [vid="iv_close"]',
+      snapshotUrls: 'https://i.gkd.li/import/14141561',
+    },
+  ],
+});
