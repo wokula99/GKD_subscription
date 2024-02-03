7397af470d25b0e6b777dc8c23216c54042d6439
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-02-03 11:35:40 +0800

    feat(车来了): 全屏广告-广告弹窗

--- src/apps/com.ygkj.chelaile.standard.ts
+++ src/apps/com.ygkj.chelaile.standard.ts
@@ -10,6 +10,7 @@ export default defineAppConfig({
       rules: [
         {
           key: 0,
+          name: '腾讯SDK',
           activityIds: [
             'dev.xesam.chelaile.app.module.func.SplashActivity',
             'dev.xesam.chelaile.app.module.PanelHostActivity',
@@ -26,10 +27,31 @@ export default defineAppConfig({
         },
         {
           key: 1,
-          activityIds: 'dev.xesam.chelaile.app.module.PanelHostActivity',
+          activityIds: [
+            'dev.xesam.chelaile.app.module.PanelHostActivity',
+            'dev.xesam.chelaile.app.module.line.TimeTableActivity',
+          ],
           matches:
             '[id="com.ygkj.chelaile.standard:id/cll_line_single_ad_close"][visibleToUser=true]',
-          snapshotUrls: 'https://i.gkd.li/import/13625374',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13625374',
+            'https://i.gkd.li/import/13947849',
+          ],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '全屏广告-广告弹窗',
+      rules: [
+        {
+          key: 0,
+          name: '腾讯广告SDK',
+          activityIds:
+            'dev.xesam.chelaile.app.module.line.gray.LineDetailActivity',
+          matches:
+            'FrameLayout[childCount>1] > FrameLayout[childCount=1] > ImageView[width<90][height<90]',
+          snapshotUrls: 'https://i.gkd.li/import/13947871',
         },
       ],
     },
